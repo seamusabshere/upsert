a4eed1fc187d519bcdba50bc63a944faafafdbd2
Phil <pnomolos@gmail.com>
2013-06-15 13:23:41 -0500

    Remove some unnecessary comments.

--- lib/upsert/column_definition/mysql.rb
+++ lib/upsert/column_definition/mysql.rb
@@ -6,10 +6,9 @@ class Upsert
         def all(connection, table_name)
           columns = connection.execute("SHOW COLUMNS FROM #{connection.quote_ident(table_name)}").map do |row|
             # {"Field"=>"name", "Type"=>"varchar(255)", "Null"=>"NO", "Key"=>"PRI", "Default"=>nil, "Extra"=>""}
-            # 
-            name = row['Field'] || row['COLUMN_NAME'] # || row[:Field] || row[:COLUMN_NAME]
-            type = row['Type'] || row['COLUMN_TYPE'] # || row[:Type] || row[:COLUMN_TYPE]
-            default = row['Default'] || row['COLUMN_DEFAULT'] # || row[:Default] || row[:COLUMN_DEFAULT]
+            name = row['Field'] || row['COLUMN_NAME']
+            type = row['Type'] || row['COLUMN_TYPE']
+            default = row['Default'] || row['COLUMN_DEFAULT']
             new connection, name, type, default
           end.sort_by do |cd|
             cd.name
