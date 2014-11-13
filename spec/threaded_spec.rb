b87ea669f549740e543dda4330ad123ea12bd9ba
Thibaut Barrère <thibaut.barrere@gmail.com>
2014-11-13 20:55:44 +0100

    Add a spec to reproduce thread-safety on upsert

--- spec/threaded_spec.rb
+++ spec/threaded_spec.rb
@@ -1,21 +1,33 @@
 require 'spec_helper'
 describe Upsert do
   describe "is thread-safe" do
-    it "is safe to use one-by-one" do
-      upsert = Upsert.new $conn, :pets
+    it "for one-by-one use once function has been created" do
+      # first make sure to create the function separately (this is not thread-safe on its own)
+      ActiveRecord::Base.connection_pool.with_connection do |connection|
+        upsert = Upsert.new(connection, :pets, assume_function_exists: false)
+        upsert.row({name: 'xxx'}, gender: "blah")
+      end
+      
+      # then get a failure because of connection reuse
       assert_creates(Pet, [{:name => 'Jerry', :gender => 'neutered'}]) do
         ts = []
         10.times do
           ts << Thread.new do
-            sleep 0.2
-            upsert.row({:name => 'Jerry'}, :gender => 'male')
-            upsert.row({:name => 'Jerry'}, :gender => 'neutered')
+            ActiveRecord::Base.connection_pool.with_connection do |connection|
+              upsert = Upsert.new(connection, :pets, assume_function_exists: true)
+              upsert.row({:name => 'Jerry'}, :gender => 'male')
+              upsert.row({:name => 'Jerry'}, :gender => 'neutered')
+            end
           end
-          ts.each { |t| t.join }
         end
+        ts.each { |t| t.join }
       end
     end
+    
+    it "for function creation" # to be implemented, but need to delete existing functions first!
+
     it "is safe to use batch" do
+      pending "temporarily disable to underline the main problem"
       assert_creates(Pet, [{:name => 'Jerry', :gender => 'neutered'}]) do
         Upsert.batch($conn, :pets) do |upsert|
           ts = []
