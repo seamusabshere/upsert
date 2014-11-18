b9f8ee01dce5fde628b36de0ee9b383b000e9aab
Thibaut Barrère <thibaut.barrere@gmail.com>
2014-11-18 17:59:20 +0100

    Adjust specifications

--- spec/threaded_spec.rb
+++ spec/threaded_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 describe Upsert do
+  before do
+    # first make sure to create the function separately (this is not thread-safe on its own)
+    ActiveRecord::Base.connection_pool.with_connection do |connection|
+      upsert = Upsert.new(connection, :pets, assume_function_exists: false)
+      upsert.row({name: 'xxx'}, gender: "blah")
+    end
+  end
+  
   describe "is thread-safe" do
-    it "for one-by-one use once function has been created" do
-      # first make sure to create the function separately (this is not thread-safe on its own)
-      ActiveRecord::Base.connection_pool.with_connection do |connection|
-        upsert = Upsert.new(connection, :pets, assume_function_exists: false)
-        upsert.row({name: 'xxx'}, gender: "blah")
-      end
-      
+    it "for one-by-one use once function has been created", focus: true do
       # then get a failure because of connection reuse
       assert_creates(Pet, [{:name => 'Jerry', :gender => 'neutered'}]) do
         ts = []
@@ -27,19 +29,19 @@ describe Upsert do
     it "for function creation" # to be implemented, but need to delete existing functions first!
 
     it "is safe to use batch" do
-      pending "temporarily disable to underline the main problem"
       assert_creates(Pet, [{:name => 'Jerry', :gender => 'neutered'}]) do
-        Upsert.batch($conn, :pets) do |upsert|
-          ts = []
-          10.times do
-            ts << Thread.new do
-              sleep 0.2
-              upsert.row({:name => 'Jerry'}, :gender => 'male')
-              upsert.row({:name => 'Jerry'}, :gender => 'neutered')
+        ts = []
+        10.times do
+          ts << Thread.new do
+            ActiveRecord::Base.connection_pool.with_connection do |connection|
+              Upsert.batch(connection, :pets, assume_function_exists: true) do |upsert|
+                upsert.row({:name => 'Jerry'}, :gender => 'male')
+                upsert.row({:name => 'Jerry'}, :gender => 'neutered')
+              end
             end
-            ts.each { |t| t.join }
           end
         end
+        ts.each { |t| t.join }
       end
     end
   end
