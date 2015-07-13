b996149be8a62b47e8afbb5fd986113fe4c19b5c
Zak Crawford <zcrawford@angelrush.com>
2015-07-13 11:28:32 -0700

    two dot one dot one

--- lib/upsert.rb
+++ lib/upsert.rb
@@ -231,7 +231,7 @@ class Upsert
 
   # @private
   def quoted_table_name
-    @quoted_table_name ||= connection.quote_ident table_name.gsub('"', '').split('.')
+    @quoted_table_name ||= connection.quote_ident(table_name.gsub('"', '').split('.'))
   end
 
   # @private
