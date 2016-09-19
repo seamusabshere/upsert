8e800843dffea81f053c5474a7d14bbaff1e20ab
Phil <pnomolos@gmail.com>
2016-09-19 15:50:18 -0700

    Move DB=postgresql check to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,8 @@ require 'active_record_inline_schema'
 
 require 'activerecord-import' if RUBY_VERSION >= '1.9'
 
-ENV['DB'] ||= 'postgresql'
+ENV['DB'] = 'postgresql' if ENV['DB'] =~ /postgresql/
+ENV['DB'] ||= 'mysql'
 
 class RawConnectionFactory
   DATABASE = 'upsert_test'
