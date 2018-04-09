a9f68ade711d1b8010a35533b61d7f35af64203c
Phil <pnomolos@gmail.com>
2018-04-09 09:21:52 -0700

    Fix spec for jruby

--- spec/type_safety_spec.rb
+++ spec/type_safety_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 describe Upsert do
   describe "type safety" do
     it "does not attempt to typecast values" do
+      error_class = RUBY_PLATFORM == 'java' ? org.postgresql.util.PSQLException : PG::InvalidTextRepresentation
       upsert = Upsert.new $conn, :pets
       lambda do
         upsert.row :tag_number => ''
-      end.should raise_error PG::InvalidTextRepresentation
+      end.should raise_error error_class
     end
   end
 end if ENV['DB'] == 'postgresql'
