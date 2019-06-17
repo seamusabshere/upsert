26452916e62e79ce82a0dd31cc05fc7a02af253b
Phil <pnomolos@gmail.com>
2019-06-17 01:21:37 -0700

    Fix script

--- travis/run_specs.sh
+++ travis/run_specs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(rvm info | grep jruby | wc -l)" -gt 5 ]; then
-  export CLASSPATH="$(bundle show jdbc-postgres)/lib/postgresql-42.1.4.jar"`;
+  export CLASSPATH="$(bundle show jdbc-postgres)/lib/postgresql-42.1.4.jar";
 fi
 # https://markmail.org/message/qb32j3ybhhu7bfge#query:+page:1+mid:qb32j3ybhhu7bfge+state:results
 bundle exec rake spec
